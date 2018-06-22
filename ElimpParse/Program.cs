83200bdd885b4f85b626da17f57e3f85be569904
Fredi Kats <fredikats@outlook.com>
2018-06-22 09:32:33 +0300

    Add project files.

---
+++ ElimpParse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElimpParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
