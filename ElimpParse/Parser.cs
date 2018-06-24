eb808168dc76fc93bf0aac303c91bd7e74c97399
Unknown <moskovets1965@gmail.com>
2018-06-24 21:57:01 +0300

    бот для телеги

--- ElimpParse/Parser.cs
+++ ElimpParse/Parser.cs
@@ -22,7 +22,7 @@ namespace ElimpParse
             var floatRow = _client.Load(link)
                 .DocumentNode
                 .SelectSingleNode("//*[contains(@class,'eo-flex-row')]");
-             
+
             return int.Parse(floatRow
                 .ChildNodes[1]
                 .ChildNodes[0]
--- ElimpParse/Program.cs
+++ ElimpParse/Program.cs
@@ -2,12 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telegram;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+
 
 namespace ElimpParse
 {
     class Program
     {
+        public static readonly TelegramBotClient Bot = new TelegramBotClient("574977062:AAHC1xLdYfEtZ8EYQYuprhAi3DJYDhcgeGw");
         static void Main(string[] args)
+        {
+
+            /* var oldPlayers = new List<ElimpUser>
+             {
+                 new ElimpUser("Strannik", "II место на области"),
+                 new ElimpUser("iNooByX", "III место на области"),
+                 new ElimpUser("Maxkolpak", "III место на городе"),
+                 new ElimpUser("krab397", "III место на облати"),
+                 new ElimpUser("i4happy", "I место на городе"),
+                 new ElimpUser("vlad986523", "II место на городе")
+             };
+             */
+            //   users.InsertRange(0, oldPlayers);
+            Bot.OnMessage += Bot_OnMassage;
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        public static string PrintResult(List<ElimpUser> users, bool isHtml)
+        {
+            foreach (var user in users)
+            {
+                user.ComplitedTaskCount = Parser.ComplitedTaskCount(user.Login);
+            }
+
+            var sorted = users.OrderByDescending(e => e.ComplitedTaskCount).ToList();
+            string s = "";
+            for (var i = 0; i < sorted.Count; i++)
+            {
+                if (isHtml)
+                {
+                    return $"<tr><td>{i}.</td><td>{sorted[i].Login}</td><td>{sorted[i].ComplitedTaskCount}</td></tr>";
+                }
+                else
+                {
+                    s += $"<code>{sorted[i]}</code>\n";
+                    Console.WriteLine($"{sorted[i]}\n");
+                }
+            }
+            return s;
+
+        }
+        public static void Bot_OnMassage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             var users = new List<ElimpUser>()
             {
@@ -26,41 +78,17 @@ namespace ElimpParse
                 new ElimpUser("v_7946"),
                 new ElimpUser("Versuzzz"),
                 new ElimpUser("Xsqten"),
-                new ElimpUser("Enosha", "Нет места на всеукре :с")
+                new ElimpUser("Enosha")
             };
-            var oldPlayers = new List<ElimpUser>
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
-                new ElimpUser("Strannik", "II место на области"),
-                new ElimpUser("iNooByX", "III место на области"),
-                new ElimpUser("Maxkolpak", "III место на городе"),
-                new ElimpUser("krab397", "III место на облати"),
-                new ElimpUser("i4happy", "I место на городе"),
-                new ElimpUser("vlad986523", "II место на городе")
-            };
-            users.InsertRange(0, oldPlayers);
-            PrintResult(users, false);
-            
-        }
-
-        public static void PrintResult(List<ElimpUser> users, bool isHtml)
-        {
-            foreach (var user in users)
-            {
-                user.ComplitedTaskCount = Parser.ComplitedTaskCount(user.Login);
-            }
-
-            var sorted = users.OrderByDescending(e => e.ComplitedTaskCount).ToList();
-            for (var i = 0; i < sorted.Count; i++)
-            {
-                if (isHtml)
+                if (e.Message.Text == "/getinfo")
                 {
-                    Console.WriteLine($"<tr><td>{i}.</td><td>{sorted[i].Login}</td><td>{sorted[i].ComplitedTaskCount}</td></tr>");
-                }
-                else
-                {
-                    Console.WriteLine($"{i}. {sorted[i]}");
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, PrintResult(users, false), ParseMode.Html);
+                    Console.WriteLine("good");
                 }
             }
+
         }
     }
 }
