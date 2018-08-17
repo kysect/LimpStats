8755b46d7e3ae2860b8ad4db4126fc1fa20f82d4
Unknown <moskovets1965@gmail.com>
2018-08-17 14:48:24 +0300

    add grid, button refresh, and fun button

---
+++ LimpStats.Client/GridCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimpStats.Client
+{
+    class GridCard
+    {
+        public GridCard(string Name, string Nickname, int TotalPoints)
+        {
+            this.Name = Name;
+            this.Nickname = Nickname;
+            this.TotalPoints = TotalPoints;
+        }
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public int TotalPoints { get; set; }
+    }
+}
