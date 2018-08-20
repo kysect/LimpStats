999e9aec631daa5056e0cab6fa5801d269e91dd0
Unknown <moskovets1965@gmail.com>
2018-08-20 17:48:38 +0300

    add dynamicly validation login

--- LimpStats.Client/MainWindow.xaml.cs
+++ LimpStats.Client/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace LimpStats.Client
         public MainWindow()
         {
             InitializeComponent();
+
         }
         private void button1_Click(object sender, EventArgs e)
         {
