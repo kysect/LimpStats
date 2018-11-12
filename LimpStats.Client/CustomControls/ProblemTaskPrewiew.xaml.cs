e774264b343edee09dd6a09eb47d712e15f162a3
Unknown <moskovets1965@gmail.com>
2018-11-12 23:52:36 +0200

    up

---
+++ LimpStats.Client/CustomControls/ProblemTaskPrewiew.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LimpStats.Client.CustomControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ProblemTaskPrewiew.xaml
+    /// </summary>
+    public partial class ProblemTaskPrewiew : UserControl
+    {
+        private ProblemPackWindow _problemPackWindow;
+        public ProblemTaskPrewiew(ProblemPackWindow problemPackWindow)
+        {
+            InitializeComponent();
+            _problemPackWindow = problemPackWindow;
+        }
+        private void FilePath_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                _problemPackWindow.Panel.Children.Add(new ProblemTaskPrewiew(_problemPackWindow));
+            }
+        }
+    }
+}
--- LimpStats.Client/ProblemPackWindow.xaml.cs
+++ LimpStats.Client/ProblemPackWindow.xaml.cs
@@ -25,56 +25,7 @@ namespace LimpStats.Client
         public ProblemPackWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            var f = new InitializationCardWindow(new StudyGroup());
-            f.ShowDialog();
-        }
-
-        public void OnClick_UpdatePanel(object sender, RoutedEventArgs e)
-        {
-           // Panel.Children.Add(new StudentGroupPreview(this, FilePath.Text));
-            FilePath.Text = string.Empty;
-            PanelViewer.ScrollToRightEnd();
-        }
-        private void LoadFromFile_ButtonClick(string title)
-        {
-           // var preview = new StudentGroupPreview(this, title);
-           // preview.Update();
-           // Panel.Children.Add(preview);
-            PanelViewer.ScrollToRightEnd();
-
-        }
-        private void TextBox1_OnGotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox tb = (TextBox)sender;
-            tb.Text = string.Empty;
-            tb.GotFocus -= TextBox1_OnGotFocus;
-        }
-
-        private void FilePath_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox tb = (TextBox)sender;
-            AddList.IsEnabled = tb.Text != string.Empty;
-        }
-
-        private void FilePath_OnKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                OnClick_UpdatePanel(new object(), new RoutedEventArgs());
-            }
-        }
-
-        private void PanelViewer_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            var s = JsonBackupManager.LoadCardName();
-            foreach (var title in s)
-            {
-                LoadFromFile_ButtonClick(title);
-            }
-        }
+            Panel.Children.Add(new ProblemTaskPrewiew(this));
+        }   
     }
 }
