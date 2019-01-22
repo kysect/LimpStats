2056d5fa893b41156e56db182419f069c7da6808
Fredi Kats <fredikats@outlook.com>
2019-01-22 21:34:13 +0300

    Update nugets + add CodeforcesApiWrapper

---
+++ LimpStats.Core/CodeforcesParser/CodeforcesProfileParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeforcesApiWrapper;
+using CodeforcesApiWrapper.Types;
+
+namespace LimpStats.Core.CodeforcesParser
+{
+    public static class CodeforcesProfileParser
+    {
+        private static readonly Codeforces CodeforcesClient = new Codeforces();
+
+        public static List<string> GetUserSolvedProblem(string handle)
+        {
+            ResponseContainer<List<Submission>> response = CodeforcesClient
+                .User
+                .Status(handle)
+                .Result;
+
+            response.ThrowExceptionIfFailed();
+
+            IEnumerable<Submission> okSubmission = response
+                .Result
+                .Where(s => s.Verdict == SubmissionVerdictEnum.Ok);
+
+            IEnumerable<string> solvedProblems = okSubmission
+                .Select(s => $"{s.Problem.ContestId}{s.Problem.Index}")
+                .Distinct();
+
+            return solvedProblems.ToList();
+        }
+
+        private static void ThrowExceptionIfFailed<T>(this ResponseContainer<T> response)
+        {
+            if (response.Status == "FAILED")
+            {
+                throw new Exception(response.Comment);
+            }
+        }
+    }
+}
