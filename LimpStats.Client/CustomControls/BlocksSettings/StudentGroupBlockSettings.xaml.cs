dba347ab8cb1cc882f6539ec47c104a134f5d3fc
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-03-05 01:43:33 +0300

    Implement UI-update on delete click

--- LimpStats.Client/CustomControls/BlocksSettings/StudentGroupBlockSettings.xaml.cs
+++ LimpStats.Client/CustomControls/BlocksSettings/StudentGroupBlockSettings.xaml.cs
@@ -1,34 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using LimpStats.Client.CustomControls.ForStudents;
+using LimpStats.Client.Tools;
 using LimpStats.Database;
+using LimpStats.Model;
 
 namespace LimpStats.Client.CustomControls.BlocksSettings
 {
-    /// <summary>
-    /// Логика взаимодействия для StudentGroupBlockSettings.xaml
-    /// </summary>
     public partial class StudentGroupBlockSettings : UserControl
     {
         public StudentGroupBlockSettings()
         {
             InitializeComponent();
-            var groups = DataProvider.UserGroupRepository.ReadAll();
-            foreach (var group in groups)
+            UpdateUi();
+        }
+
+        public void UpdateUi()
+        {
+            ThreadingTools.ExecuteUiThread(() => GroupListPanel.Children.Clear());
+            List<UserGroup> groups = DataProvider.UserGroupRepository.ReadAll();
+            foreach (UserGroup group in groups)
             {
-                GroupListPanel.Children.Add(new StudentGroupSettings(group));
+                var settings = new StudentGroupSettings(group, UpdateUi);
+                ThreadingTools.ExecuteUiThread(() => GroupListPanel.Children.Add(settings));
             }
         }
     }
