a0fc50ae1307e4029ce91603d7a62e1556378234
Step1 <moskovets1965@gmail.com>
2019-04-16 23:02:14 +0300

    add excel to ResGrid

--- LimpStats.Client/CustomControls/BlocksPrewiew/ResultGridBlock.xaml.cs
+++ LimpStats.Client/CustomControls/BlocksPrewiew/ResultGridBlock.xaml.cs
@@ -1,36 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.Data;
+using System.Windows;
 using System.Windows.Controls;
 using LimpStats.Model;
 using LimpStats.Model.Problems;
+using System.IO;
+using OfficeOpenXml;
+using Microsoft.Win32;
 
 namespace LimpStats.Client.CustomControls.BlocksPrewiew
 {
     public partial class ResultGridBlock : UserControl
     {
+        private ProblemsPack _pack;
+        private List<LimpUser> _users;
         public ResultGridBlock(List<LimpUser> users, ProblemsPack pack)
         {
             InitializeComponent();
-            DataTable table = InitDataTable(pack, users);
+            _pack = pack;
+            _users = users;
+            DataTable table = InitDataTable();
             
             ResGrid.ItemsSource = table.DefaultView;
         }
 
-        private DataTable InitDataTable(ProblemsPack pack, List<LimpUser> users)
+        private DataTable InitDataTable()
         {
             var table = new DataTable();
 
             //TODO: Нужно будет потом все подобные строки вынести отдельно, чтобы изменять можно было нормально
             table.Columns.Add("Name");
-            foreach (Problem problem in pack.Problems)
+            foreach (Problem problem in _pack.Problems)
             {
                 table.Columns.Add(problem.Title);
             }
 
-            foreach (LimpUser user in users)
+            foreach (LimpUser user in _users)
             {
                 var data = new List<object> {user.Username};
-                foreach (Problem problem  in pack.Problems)
+                foreach (Problem problem  in _pack.Problems)
                 {
                     data.Add(problem.GetUserResult(user));
                 }
@@ -39,6 +47,47 @@ namespace LimpStats.Client.CustomControls.BlocksPrewiew
             }
 
             return table;
+        }
+
+        private void SaveToExcelButton_Click(object sender, RoutedEventArgs e)
+        {
+      
+           
+            using (var excel = new ExcelPackage(new FileInfo(SaveFileDialog() + ".xlsx")))
+            {
+                var ws = excel.Workbook.Worksheets.Add("StudentGroup");
+                int i = 2;
+                foreach (Problem problem in _pack.Problems)
+                {
+                    ws.Cells[1, i].Value = problem.Title;
+                    i++;
+                }
+                i = 2;
+                foreach (LimpUser user in _users)
+                {
+                    int k = 2;
+                    ws.Cells[i, 1].Value = user.Username;
+                    foreach (Problem problem in _pack.Problems)
+                    {
+                        ws.Cells[i, k].Value = problem.GetUserResult(user);
+                        k++;
+                    }           
+                    i++;
+                }
+               
+                excel.Save();
+            }
+
+
+        }
+        private static string SaveFileDialog()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                return saveFileDialog.FileName;
+            }
+            return null;
         }
     }
 }
